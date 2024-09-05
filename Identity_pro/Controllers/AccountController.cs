17ae507ab679fff1cbe903624d0bf96a478ff94c
mahmoudqtari <mahmoudqtari5@gmail.com>
2024-09-05 20:40:32 +0300

    create simple identity user

---
+++ Identity_pro/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Identity_pro.Data;
+using Identity_pro.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity_pro.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signinManager;
+
+        public AccountController(ApplicationDbContext context,UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signinManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.signinManager = signinManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                UserName = model.Email
+            };
+            var result = await userManager.CreateAsync(user,model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel VmModel)
+        {
+            var result = await signinManager.PasswordSignInAsync(VmModel.Email, VmModel.Password, VmModel.RememberMe, false);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(VmModel);
+        }
+    }
+}
---
+++ Identity_pro/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity_pro.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Phone { get; set; }
+    }
+}
